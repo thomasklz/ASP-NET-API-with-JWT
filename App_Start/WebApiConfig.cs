62c47639e3169f8319af81bb5a2fc45a59b73f36
thomasklz <pipos_rgt@hotmail.com>
2021-02-02 14:04:22 -0500

    Guia ASP.NET JWToken

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebApplication6.Controllers;
+
+namespace WebApplication6
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración de rutas y servicios de API
+            config.MapHttpAttributeRoutes();
+
+            config.MessageHandlers.Add(new TokenValidationHandler());
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
